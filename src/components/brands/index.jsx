859a13cafd2b45a2c0fd846f2de663b87d6da395
Azamat <azamatkhabibullaevcto@gmail.com>
2024-01-01 14:36:13 +0500

    Fix image url

--- src/components/brands/index.jsx
+++ src/components/brands/index.jsx
@@ -1,3 +1,8 @@
+import Logo1 from '@/assets/images/logo1.png'
+import Logo2 from '@/assets/images/logo2.png'
+import Logo3 from '@/assets/images/logo3.png'
+import Logo4 from '@/assets/images/logo4.png'
+
 function Brands() {
   return (
     <section className="pt-[4.5rem] pb-8">
@@ -7,28 +12,28 @@ function Brands() {
             <div>
               <img
                 className="h-[60px] opacity-[0.2] transition-opacity hover:opacity-[0.6]"
-                src="/src/assets/images/logo1.png"
+                src={Logo1}
                 alt="Prologics"
               />
             </div>
             <div>
               <img
                 className="h-[60px] opacity-[0.2] transition-opacity hover:opacity-[0.6]"
-                src="/src/assets/images/logo2.png"
+                src={Logo2}
                 alt="American Tower"
               />
             </div>
             <div>
               <img
                 className="h-[60px] opacity-[0.2] transition-opacity hover:opacity-[0.6]"
-                src="/src/assets/images/logo3.png"
+                src={Logo3}
                 alt="Equinix"
               />
             </div>
             <div>
               <img
                 className="h-[60px] opacity-[0.2] transition-opacity hover:opacity-[0.6]"
-                src="/src/assets/images/logo4.png"
+                src={Logo4}
                 alt="Digital Reality"
               />
             </div>
